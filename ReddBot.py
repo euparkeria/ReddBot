9cd1ce0479ee82f4a7c200093c43c1c6775c839c
mekoneko <atamagahennaneko@gmail.com>
2014-03-08 11:17:34 +0200

    Reactionary brigade detection

--- ReddBot.py
+++ ReddBot.py
@@ -32,11 +32,16 @@ class ReddBot:
 
     @staticmethod
     def connect_to_socialmedia(authinfo, useragent):
-        r = praw.Reddit(user_agent=useragent, api_request_delay=1)
-        r.login(authinfo['REDDIT_BOT_USERNAME'], authinfo['REDDIT_BOT_PASSWORD'])
-
-        t = Twython(authinfo['APP_KEY'], authinfo['APP_SECRET'],
-                    authinfo['OAUTH_TOKEN'], authinfo['OAUTH_TOKEN_SECRET'])
+        try:
+            r = praw.Reddit(user_agent=useragent, api_request_delay=1)
+            r.login(authinfo['REDDIT_BOT_USERNAME'], authinfo['REDDIT_BOT_PASSWORD'])
+        except:
+            print('ERROR: Cant login to Reddit.com')
+        try:
+            t = Twython(authinfo['APP_KEY'], authinfo['APP_SECRET'],
+                        authinfo['OAUTH_TOKEN'], authinfo['OAUTH_TOKEN_SECRET'])
+        except:
+            print('ERROR: Cant authenticate into twitter')
         return r, t
 
     def __init__(self, useragent, authfilename, datafilename):
@@ -49,36 +54,35 @@ class ReddBot:
         self.permcounters = {'comments': 0, 'submissions': 0}
 
         while True:
-            try:
-                if os.stat(datafilename).st_mtime > self.data_modified_time:  # check if config file has changed
-                    self.redd_data, self.bot_auth_info = self.readconfig(authfilename, datafilename)
-                    self.reddit_session, self.twitter = self.connect_to_socialmedia(self.bot_auth_info,
-                                                                                    useragent=useragent)
 
-                self.cont_num['submissions'] = 0
-                self.cont_num['comments'] = 0
+            if os.stat(datafilename).st_mtime > self.data_modified_time:  # check if config file has changed
+                self.redd_data, self.bot_auth_info = self.readconfig(authfilename, datafilename)
+                self.reddit_session, self.twitter = self.connect_to_socialmedia(self.bot_auth_info,
+                                                                                useragent=useragent)
 
-                for loop in self.loops:
-                    self.contentloop(target=loop)
-                    if len(self.already_done[loop]) >= buffer_reset_lenght:
-                        self.already_done[loop] = self.already_done[loop][int(len(self.already_done[loop]) / 2):]
-                        self.debug('DEBUG:buffers LENGHT after trim {0}'.format(len(self.already_done[loop])))
-                    if not self.first_run:
-                        self.pulllimit[loop] = self._calculate_pull_limit(self.cont_num[loop], target=loop)
-                    self.permcounters[loop] += self.cont_num[loop]
+            self.cont_num['submissions'] = 0
+            self.cont_num['comments'] = 0
 
-                self.debug('Running for :{0} secs. Submissions so far: {1}, THIS run: {2}.'
-                           ' Comments so  far:{3}, THIS run:{4}'
-                           .format(int((time.time() - start_time)), self.permcounters['submissions'],
-                                   self.cont_num['submissions'], self.permcounters['comments'],
-                                   self.cont_num['comments']))
+            for loop in self.loops:
+                self.contentloop(target=loop)
+                if len(self.already_done[loop]) >= buffer_reset_lenght:
+                    self.already_done[loop] = self.already_done[loop][int(len(self.already_done[loop]) / 2):]
+                    self.debug('DEBUG:buffers LENGHT after trim {0}'.format(len(self.already_done[loop])))
+                if not self.first_run:
+                    self.pulllimit[loop] = self._calculate_pull_limit(self.cont_num[loop], target=loop)
+                self.permcounters[loop] += self.cont_num[loop]
 
-                self.first_run = False
+            self.debug('Running for :{0} secs. Submissions so far: {1}, THIS run: {2}.'
+                       ' Comments so  far:{3}, THIS run:{4}'
+                       .format(int((time.time() - start_time)), self.permcounters['submissions'],
+                               self.cont_num['submissions'], self.permcounters['comments'],
+                               self.cont_num['comments']))
 
-                self.debug(self.pulllimit['submissions'])
-                self.debug(self.pulllimit['comments'])
-            except:
-                print('HTTP Error')
+            self.first_run = False
+
+            self.debug(self.pulllimit['submissions'])
+            self.debug(self.pulllimit['comments'])
+
             time.sleep(loop_timer)
             
     def _calculate_pull_limit(self, lastpullnum, target):
@@ -96,10 +100,13 @@ class ReddBot:
         return int(self.pulllimit[target])
 
     def contentloop(self, target):
-        if target == 'submissions':
-            results = self.reddit_session.get_subreddit(watched_subreddit).get_new(limit=self.pulllimit[target])
-        if target == 'comments':
-            results = self.reddit_session.get_comments(watched_subreddit, limit=self.pulllimit[target])
+        try:
+            if target == 'submissions':
+                results = self.reddit_session.get_subreddit(watched_subreddit).get_new(limit=self.pulllimit[target])
+            if target == 'comments':
+                results = self.reddit_session.get_comments(watched_subreddit, limit=self.pulllimit[target])
+        except:
+            print('ERROR: Cant connect to reddit, may be down.')
 
         for content in results:
             if content.id not in self.already_done[target]:
@@ -142,7 +149,7 @@ class ReddBot:
                                                     '*I am a bot, please PM if this message is a mistake.* \n\n'
                                                     .format(dsubmission.subreddit, dsubmission.short_link))
                             except:
-                                print('brigade warning failed')
+                                print('brigade warning failed, cant comment')
                         else:
                             msg = 'Submission regarding #{0} posted in /r/{1} : {2} #reddit'.format(
                                 item, dsubmission.subreddit, dsubmission.short_link)
@@ -150,7 +157,10 @@ class ReddBot:
                         msg = msg[:-8]
                         self.debug('MSG exceeding 140 characters!!')
                     #self.reddit_session.send_message(bot_auth_info['REDDIT_PM_TO'], 'New {0} discussion!'.format(item), msg)
-                    self.twitter.update_status(status=msg)
+                    try:
+                        self.twitter.update_status(status=msg)
+                    except:
+                        print('ERROR: couldnt update twitter status')
 
                     return 'New Topic match in:{0}, keyword:{1}'.format(dsubmission.subreddit, item)
             return False
